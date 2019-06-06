00d380751806319524667e54420db471f4f71626
umesh11111 <43033939+umesh11111@users.noreply.github.com>
2019-06-06 15:11:56 +0530

    Update tests.py

--- API/tests.py
+++ API/tests.py
@@ -1,5 +1,24 @@
 from django.test import TestCase
+from django.utils import unittest
+from django.test.client import RequestFactory
+
 
 # Create your tests here.
-class ModelTest(TestCase):
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test1(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('https://reqres.in/api/users/2')
+
+        response = getrequest(request)
+        self.assertEqual(response.status_code, 200)
+     
+    def test2(self):
+        request = self.factory.get('https://reqres.in/api/users')
+
+        response = postrequest(request)
+        self.assertEqual(response.status_code, 200)
 
